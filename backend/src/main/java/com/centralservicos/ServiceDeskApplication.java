cae45393f9e19bb58e5229679e03a63919e2fac4
Lucas Iankoski <lucas@digihub.com.br>
2026-09-01 22:04:28 -0300

    build(api): configura Spring Boot e esquema de dados

---
+++ backend/src/main/java/com/centralservicos/ServiceDeskApplication.java
@@ -0,0 +1,14 @@
+package com.centralservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class ServiceDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDeskApplication.class, args);
+    }
+}
